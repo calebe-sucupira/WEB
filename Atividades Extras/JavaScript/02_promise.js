df4edb235d67c7edc9c7249f5a6fd75a5edec3d3
calebe-sucupira <calebesucupira.o@gmail.com>
2025-07-07 11:05:04 -0300

    add

--- Atividades Extras/JavaScript/02_promise.js
+++ Atividades Extras/JavaScript/02_promise.js
@@ -31,3 +31,100 @@ function myClient() {
 }
 
 myClient();
+
+
+function obterUsuario() {
+    return new Promise(
+        (resolve) => {
+            setTimeout(
+                () => {
+                    const usuario = {
+                        id: 1,
+                        nome: "Jhon Doe",
+                        email: "jhon.doe@example.com"
+                    }
+                    resolve(usuario);
+                }
+                ,2000
+            ) 
+        }
+    )
+}
+
+obterUsuario()
+    .then(
+        (usuarioRecebido) => {
+            console.log("Usuário Encontrado com sucesso:")
+            console.log("ID: ", usuarioRecebido.id)
+            console.log("Nome: ", usuarioRecebido.nome)
+            console.log("Email: ", usuarioRecebido.email)   
+        }
+    )
+    .catch(
+        (erro) => {
+            console.error("Erro ao obter usuário:", erro);
+        }
+    )
+
+async function obterUsuarioAsync() {
+    try {
+        const usuario = await obterUsuario()
+        console.log("Usuário Encontrado com sucesso:")
+        console.log("ID: ", usuario.id)
+        console.log("Nome: ", usuario.nome)
+        console.log("Email: ", usuario.email)
+    } catch (erro) {
+        console.error("Erro ao obter usuário:", erro)
+    }
+}
+
+obterUsuarioAsync()
+
+function verificarTemperatura(temperatura) {
+    return new Promise(
+        (resolve, reject) => {
+            if (temperatura <= 30) {
+                resolve("Tempratura Normal")
+            } else {
+                reject("Temperatura muito Alta!")
+            }
+        }
+    )
+}
+
+verificarTemperatura(35)
+    .then(
+        temperaturaRecebida => {
+            console.log(temperaturaRecebida);
+        }
+    )
+    .catch(
+        erro => {
+            console.error(erro);
+        }
+    )
+
+verificarTemperatura(25)
+    .then(
+        temperaturaRecebida => {
+            console.log(temperaturaRecebida);
+        }
+    )
+    .catch(
+        erro => {
+            console.error(erro);
+        }
+    )
+
+async function verificarTemperaturaAsync(temperatura) {
+    try {
+        const resultado = await verificarTemperatura(temperatura);
+        console.log(resultado);
+    }
+    catch (erro) {
+        console.error(erro);
+    }
+}
+
+verificarTemperaturaAsync(35)
+verificarTemperaturaAsync(25)
